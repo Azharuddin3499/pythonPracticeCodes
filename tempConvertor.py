12822661e9e1ec635f67bdcdd9b6340a206001f1
azharuddin <azharuddin.3499@gmail.com>
2024-02-02 14:21:48 +0500

    tempConvertor1

---
+++ tempConvertor.py
@@ -0,0 +1,9 @@
+print("program that will convert celsius value to fahrenheit")
+
+# if isinstance(num, (int, float)):
+#     pass
+celcius=eval(input("Enter Celcius °C: "))
+f= celcius * 9/5 + 32 
+
+print("%s °C = %s °F"%(celcius,f))
+
